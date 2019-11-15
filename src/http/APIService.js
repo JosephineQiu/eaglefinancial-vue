3f845e04ced797544ed6bce72d46547c9b224779
JosephineQiu <mqiu@unomaha.edu>
2019-11-14 19:57:12 -0600

    changed backend

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import axios from 'axios';
 
-const API_URL = 'https://efs-django-vue.herokuapp.com/'; /* http://groyce.pythonanywhere.com/ or http://localhost:8000 */
+const API_URL = 'https://groyce.pythonanywhere.com/'; /* http://groyce.pythonanywhere.com/ or http://localhost:8000 */
 
 export class APIService {
   constructor() {
